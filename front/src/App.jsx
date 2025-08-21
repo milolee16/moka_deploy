045aa48fe134220bab16b11a048ebe30a5696fc6
ungraduated <85732427+ungraduated@users.noreply.github.com>
2025-08-21 15:20:54 +0900

    edit confilcts

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,37 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-
-// 공통 레이아웃
-import Layout from "./components/Layout.jsx";
-
-// 페이지 컴포넌트
-import Welcome from "./components/Welcome.jsx";
-import Index from "./components/Index.jsx";
-import Reservation from "./components/Reservation.jsx";
-import MapPage from "./components/Map.jsx";
-import CarSelect from "./components/CarSelect.jsx";
-import InsuranceSelect from "./components/InsuranceSelect.jsx";
-import Checkout from "./components/Checkout.jsx";
-import PaymentOptions from "./components/PaymentOptions.jsx";
-import AdminPage from "./pages/AdminPage";
-
-function App() {
-  return (
-      <Routes>
-        {/* Layout 컴포넌트가 하위 모든 페이지의 공통 레이아웃을 담당합니다. */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/home" element={<Index />} />
-          <Route path="/reserve" element={<Reservation />} />
-          <Route path="/map" element={<MapPage />} />
-          <Route path="/cars" element={<CarSelect />} />
-          <Route path="/insurance" element={<InsuranceSelect />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/payment-options" element={<PaymentOptions />} />
-          <Route path="/admin/*" element={<AdminPage />} />
-        </Route>
-      </Routes>
-=======
 import Index from './components/Index.jsx';
 import Welcome from './components/Welcome.jsx';
 import Reservation from './components/Reservation.jsx';
@@ -57,7 +24,6 @@ function App() {
         <Route path="/ocr" element={<OcrPage />} />
       </Route>
     </Routes>
->>>>>>> 1094113da2b7a160ca9d0aa289b7b47bc608f5f8
   );
 }
 
